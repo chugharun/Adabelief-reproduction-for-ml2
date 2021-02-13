87c3a783fc57e02c6f62d4b25088d57ed84b3572
chugharun <71560112+chugharun@users.noreply.github.com>
2021-02-13 18:08:25 +0530

    Delete run.sh

--- WGAN/run.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python /content/Adabelief-reproduction-for-ml2/WGAN/main.py --optimizer adabelief --eps 1e-12 --Train --dataset cifar10 
-CUDA_VISIBLE_DEVICES=0 python /content/Adabelief-reproduction-for-ml2/WGAN/main.py --optimizer adam --eps 1e-12 --Train --dataset cifar10
-CUDA_VISIBLE_DEVICES=0 python /content/Adabelief-reproduction-for-ml2/WGAN/main.py --optimizer sgd  --Train --dataset cifar10
-CUDA_VISIBLE_DEVICES=0 python /content/Adabelief-reproduction-for-ml2/WGAN/main.py --optimizer adabound --eps 1e-12 --Train --dataset cifar10
