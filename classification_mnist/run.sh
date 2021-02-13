5d94bce38fe48c0a65bdcd6d54528201fadd8eb4
chugharun <71560112+chugharun@users.noreply.github.com>
2021-02-13 23:31:11 +0530

    Update run.sh

--- classification_mnist/run.sh
+++ classification_mnist/run.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=0 python /content/Adabelief-reproduction-for-ml2/classification_mnist/main.py --model vgg --optim adabelief --lr 0.001 --eps 1e-8 --beta1 0.9 --beta2 0.999 --weight_decay 0.0005
 CUDA_VISIBLE_DEVICES=0 python /content/Adabelief-reproduction-for-ml2/classification_mnist/main.py --model vgg --optim adam --lr 0.001 --eps 1e-8 --beta1 0.9 --beta2 0.999 --weight_decay 0.0005
 CUDA_VISIBLE_DEVICES=0 python /content/Adabelief-reproduction-for-ml2/classification_mnist/main.py --model vgg --optim adabound --lr 0.001 --eps 1e-8 --beta1 0.9 --beta2 0.999 --final_lr 0.1 --gamma 0.001 --weight_decay 0.0005
-CUDA_VISIBLE_DEVICES=0 python /content/Adabelief-reproduction-for-ml2/classification_mnist/main.py --model vgg --optim sgd --lr 0.1 --momentum 0.9 --weight_decay 0.0005
+CUDA_VISIBLE_DEVICES=0 python /content/Adabelief-reproduction-for-ml2/classification_mnist/main.py --model vgg --optim sgd --lr 0.01 --momentum 0.9 --weight_decay 0.0005
