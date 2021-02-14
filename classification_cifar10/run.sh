18a547ef8406008f651241b464a637d0271c802b
chugharun <71560112+chugharun@users.noreply.github.com>
2021-02-14 17:12:29 +0530

    Add files via upload

---
+++ classification_cifar10/run.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=0 python /content/Adabelief-reproduction-for-ml2/classification_cifar10/main.py --model vgg --optim adabelief --lr 1e-3 --eps 1e-8 --beta1 0.9 --beta2 0.999 --momentum 0.9 --wdecay0.0005
+CUDA_VISIBLE_DEVICES=0 python /content/Adabelief-reproduction-for-ml2/classification_cifar10/main.py --model vgg --optim adam --lr 0.001 --eps 1e-8 --beta1 0.9 --beta2 0.999 --momentum 0.9 --wdecay0.0005
+CUDA_VISIBLE_DEVICES=0 python /content/Adabelief-reproduction-for-ml2/classification_cifar10/main.py --model vgg --optim sgd --lr 0.1 --momentum 0.9 --wdecay0.0005
+CUDA_VISIBLE_DEVICES=0 python /content/Adabelief-reproduction-for-ml2/classification_cifar10/main.py --model vgg --optim adabound --lr 0.001 --eps 1e-8 --beta1 0.9 --beta2 0.999 --final_lr 0.1 --gamma 0.001 --momentum 0.9 --weight_decay 0.0005
